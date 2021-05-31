c324cf2ba887fa1d72ac29d5c51ded28f0ac48e8
Renan <renanvy@gmail.com>
2021-05-31 16:19:11 -0300

    Add styles to login page

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,14 +1,5 @@
 import React from 'react'
-import { Paper, Typography, Container } from '@material-ui/core'
 
 export default function Dashboard() {
-  return (
-    <>
-      <Container maxWidth="sm">
-        <Paper>
-          <Typography component="h1">Dashboard</Typography>
-        </Paper>
-      </Container>
-    </>
-  )
+  return <h1>Dashboard</h1>
 }
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,77 @@
-import React from 'react'
-import { Paper, Typography, Container } from '@material-ui/core'
+import React, { useState } from 'react'
+import { TextField, Button, Paper } from '@material-ui/core'
+import './styles.css'
 
 export default function Login() {
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const login = async (evt) => {
+    evt.preventDefault()
+
+    try {
+      setLoading(true)
+
+      window.location = '/'
+    } catch (error) {
+      console.error(error)
+      setLoading(false)
+    }
+  }
+
+  const updateField = (evt) => {
+    const { value } = evt.target
+
+    setData({
+      ...data,
+      [evt.target.name]: value
+    })
+  }
+
+  console.log(data)
+
   return (
-    <Container maxWidth="sm">
-      <Paper>
-        <Typography component="h1">Login</Typography>
+    <div className="Login">
+      <Paper classes={{ root: 'Login-content' }}>
+        <h1 className="Login-title">Login</h1>
+
+        <form onSubmit={login}>
+          <TextField
+            fullWidth
+            name="email"
+            label="E-mail"
+            variant="outlined"
+            margin="normal"
+            autoFocus
+            onChange={updateField}
+            value={data.email}
+          />
+
+          <TextField
+            fullWidth
+            name="password"
+            label="Senha"
+            variant="outlined"
+            margin="normal"
+            onChange={updateField}
+            value={data.password}
+          />
+
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            size="large"
+            classes={{ root: 'Login-button' }}
+            disabled={loading}
+          >
+            {loading ? 'Logando...' : 'Entrar'}
+          </Button>
+        </form>
       </Paper>
-    </Container>
+    </div>
   )
 }
