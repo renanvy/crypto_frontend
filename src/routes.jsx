63de72e7c797f41aa4ac1a1a6796e1bc55a30cec
Renan <renanvy@gmail.com>
2021-05-31 14:23:45 -0300

    Add skeleton to login page

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import DashboardPage from './pages/Dashboard'
+import LoginPage from './pages/Login'
 
 export default function Routes() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={DashboardPage} />
+        <Route path="/login" component={LoginPage} />
       </Switch>
     </Router>
   )
