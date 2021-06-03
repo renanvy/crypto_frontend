4c2f88b33a3bdd764d3b5276471fa25d97d4e319
Renan <renanvy@gmail.com>
2021-06-03 11:43:43 -0300

    Improve Loading component
    * Permit user show or hide message on loading

--- src/common_components/Loading/index.js
+++ src/common_components/Loading/index.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { CircularProgress } from '@material-ui/core'
 
 import './styles.css'
 
-export default function Loading() {
+export default function Loading({ showMessage }) {
   return (
     <div className="Loading">
       <CircularProgress />
-      <span>Carregando...</span>
+      {showMessage && <span>Carregando...</span>}
     </div>
   )
+}
+
+Loading.defaultProps = {
+  showMessage: true
+}
+
+Loading.propTypes = {
+  showMessage: PropTypes.bool
 }
